19197c60905a91f8a59655bc831eb1ebf258b4fe
DuncanGJ <duncangj@gmail.com>
2026-02-03 17:48:57 -0800

    commiting code

--- TextTransform.java
+++ TextTransform.java
@@ -1,3 +1,24 @@
-public m {
-    
+public abstract class TextTransform{
+    private String input;
+    private String output;
+
+    //setters
+    protected void setInput(String input){
+        this.input = input;
+    }
+
+    protected void setOutput(String output){
+        this.output = output;
+    }
+
+    //getters
+    public String getInput(){
+        return input;
+    }
+
+    public String getOutput(){
+        return output;
+    }
+    //abstract method to be implemented by subclasses
+    public abstract String transform(String input);
 }
--- TextTransformtest.java
+++ TextTransformtest.java
@@ -1,1 +1,68 @@
-.
+public class TextTransformtest{
+    public void main(String[] args) {
+        ReverseTransform transform = new ReverseTransform();
+        transform.transform("input");
+        System.out.println(transform.getOutput());
+
+        CaesarTransform caesar = new CaesarTransform();
+        caesar.transform("input");
+        System.out.println(caesar.getOutput());
+        caesar.transform("input", 5);
+        System.out.println(caesar.getOutput());
+        caesar.transform("input! Testing...", 12);
+        System.out.println(caesar.getOutput());
+    }
+
+    //reverse transform
+    public class ReverseTransform extends TextTransform{
+        public String transform(String uInput){
+            this.setInput(uInput);
+            //implement transform function below
+            StringBuilder builder = new StringBuilder();
+            for (int i = uInput.length()-1; i >= 0; i--){
+                builder.append(uInput.charAt(i));
+            }
+            this.setOutput(builder.toString());
+            return this.getOutput();
+        }
+    }
+    //Caesar cypher
+    public class CaesarTransform extends TextTransform{
+        public String transform(String uInput){
+            this.setInput(uInput);
+            //implement transform function below
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i <= uInput.length()-1; i++){
+                
+                builder.append((char) shiftChar(uInput.charAt(i), 5)); //+5 default value
+            }
+            this.setOutput(builder.toString());
+            return this.getOutput();
+        }
+
+        //overloading transform
+        public String transform(String uInput, int shift){
+            this.setInput(uInput);
+            //implement transform function below
+            StringBuilder builder = new StringBuilder();
+            for (int i = 0; i <= uInput.length()-1; i++){
+                builder.append((char) shiftChar(uInput.charAt(i), shift)); //let user implement the shift value
+            }
+            this.setOutput(builder.toString());
+            return this.getOutput();
+        }
+
+        private static char shiftChar(char c, int shift){
+            if (c >= 'a' && c <= 'z'){
+                int offset = c - 'a';
+                int newoffset = Math.floorMod(offset+shift, 26);
+                return (char) ('a' + newoffset);
+            }else if (c >= 'A' && c <= 'Z'){
+                int offset = c - 'a';
+                int newoffset = Math.floorMod(offset+shift, 26);
+                return (char) ('a' + newoffset);
+            }else{return c;}
+        }
+
+    }
+}
